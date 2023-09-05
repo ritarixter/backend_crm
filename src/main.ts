6518baa9e6a404a1d8d6caf0968eeaa9e4b8fc40
Яковлева Ксения Николаевна <k.yakovleva@itsl.tel>
2023-09-05 10:11:11 +0300

    промежуточный

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ async function bootstrap() {
   app.use(helmet());
   app.use(helmet.crossOriginResourcePolicy({ policy: 'cross-origin' }));
   app.enableCors({
-    origin:  ['http://frontend.corp.itsl.tel'], 
+    origin:  ['http://localhost:3000'], 
     methods: ['GET', 'POST', 'PATCH', 'DELETE', 'PUT'],
     credentials: true,
   });
